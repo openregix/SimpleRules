e3d16989989c45fdd46c72ea668e31a02dfa866b
p040186 <p040186@FRLH107256.corp.noxiane.net>
2017-12-14 15:01:44 +0100

    Initial commit

---
+++ simpleRules/src/ch/claudel/simpleRules/SimpleRules.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ch.claudel.simpleRules;
+
+/**
+ * @author p040186
+ *
+ */
+public class SimpleRules {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String msg = new String( "Intialization of SimpleRules");
+		System.out.println(msg);
+	
+	}
+
+}
